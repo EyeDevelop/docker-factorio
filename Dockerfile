7fcb056d789c1dd388238072e62aa438070e9876
Hans Goor <me@eyedevelop.org>
2021-01-01 16:50:51 +0100

    Added files.

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu
+
+RUN apt-get update -y && apt-get install -y bash curl tar xz-utils zip unzip
+
+ENV PUID 1000
+ENV PGID 1000
+ENV FACTORIO_VERSION latest
+ENV SAVENAME save
+
+COPY entrypoint.sh /entrypoint.sh
+CMD [ "/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Create a user if not done already.
+if ! grep "abc" /etc/passwd > /dev/null; then
+    groupadd --non-unique --gid "$PGID" abc
+    useradd --non-unique --no-create-home --uid "$PUID" --gid "$PGID" abc
+fi
+
+# Verify directories exist.
+mkdir -p /data
+mkdir -p /conf
+
+mkdir -p /data/saves
+
+# Install Factorio if not done already.
+if [[ ! -f /data/.factorio-setup ]]; then
+    curl -Lo /tmp/factorio.tar.gz "https://www.factorio.com/get-download/$FACTORIO_VERSION/headless/linux64"
+    mkdir -p /tmp/factorio
+    mkdir -p /data/bin
+    tar -C /tmp/factorio -xvf /tmp/factorio.tar.gz
+    mv /tmp/factorio/*/* /data/
+
+    touch /data/.factorio-setup
+fi
+
+# Set permissions.
+chown -R abc:abc /data
+chown -R abc:abc /conf
+
+# Run the Factorio server.
+if [[ ! -f "/data/saves/$SAVENAME.zip" ]]; then
+    su abc -c "/data/bin/x64/factorio --create /data/saves/$SAVENAME.zip"
+fi
+
+su abc -c "/data/bin/x64/factorio --start-server /data/saves/$SAVENAME.zip"
